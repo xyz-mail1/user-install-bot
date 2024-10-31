ff26598e334151968998ca2f86c7bc95011ecf3a
xyz-mail1 <sunday.september@protonmail.com>
2024-10-31 11:15:03 +0530

    efgrth

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require("node:path");
 
 require("dotenv").config();
 
-// Create a new client instance
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages],
 });
@@ -23,7 +22,6 @@ for (const folder of commandFolders) {
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
     } else {
@@ -34,16 +32,11 @@ for (const folder of commandFolders) {
   }
 }
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-  //if (!interaction.isChatInputCommand()) return;
-
   const command = interaction.client.commands.get(interaction.commandName);
 
   if (!command) {
@@ -69,5 +62,4 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
 });
 
-// Log in to Discord with your client's token
 client.login(process.env.token);
