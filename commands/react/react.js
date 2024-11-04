b3bc65280ecee30c880e749382e75265772f1c55
xyz-mail1 <sunday.september@protonmail.com>
2024-11-04 11:11:14 +0530

    bleh

--- commands/react/react.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("react")
-    .setDescription("react")
-    .addSubcommand((s) => s.setName("blush").setDescription("😳 *blushes*")),
-  async execute(interaction) {},
-};
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 const { log } = require("node:console");
 const fs = require("node:fs");
 const path = require("node:path");
-console.log("test");
-
 require("dotenv").config();
 
 const client = new Client({
@@ -34,34 +32,19 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
+}
 
 client.login(process.env.token);
