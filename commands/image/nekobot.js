7bfd3a969bb702c6e9c8a8b03d8a780e2152ba9a
xyz-mail1 <sunday.september@protonmail.com>
2024-11-27 06:48:56 +0530

    blehj

--- commands/image/nekobot.js
+++ commands/image/nekobot.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+} = require("discord.js");
 const fetch = require("node-fetch");
 async function button() {}
 module.exports = {
@@ -7,27 +13,33 @@ module.exports = {
     .setDescription("NSFW content (50% chance of working)")
     .setIntegrationTypes([0, 1])
     .setContexts([0, 1, 2])
-    .addSubcommands((cmd) => [
-      cmd.setName("feet").setDescription("Feet pics"),
-      cmd.setName("hentai").setDescription("Hentai"),
-      cmd.setName("ass").setDescription("Ass"),
-      cmd.setName("pgif").setDescription("Porn gif"),
-      cmd.setName("thigh").setDescription("Thighs"),
-      cmd.setName("boobs").setDescription("Boobs"),
-      cmd.setName("pussy").setDescription("Pussy"),
-      cmd.setName("gonewild").setDescription("Gonewild"),
-    ]),
+    .addSubcommand((cmd) => cmd.setName("feet").setDescription("Feet pic"))
+    .addSubcommand((cmd) => cmd.setName("hentai").setDescription("Hentai pic"))
+    .addSubcommand((cmd) => cmd.setName("ass").setDescription("Ass pic"))
+    .addSubcommand((cmd) => cmd.setName("pgif").setDescription("Porn gif"))
+    .addSubcommand((cmd) => cmd.setName("thigh").setDescription("Thigh pic"))
+    .addSubcommand((cmd) => cmd.setName("boobs").setDescription("Boobs pic"))
+    .addSubcommand((cmd) => cmd.setName("pussy").setDescription("Pussy pic"))
+    .addSubcommand((cmd) =>
+      cmd.setName("gonewild").setDescription("Gonewild pic")
+    ),
   async execute(interaction) {
     const subcommand = interaction.options.getSubcommand();
     const apiUrl = `https://nekobot.xyz/api/image?type=${subcommand}`;
 
     try {
+      const button = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("nekobot-button")
+          .setLabel("Refresh")
+          .setStyle(ButtonStyle.Success)
+      );
       const res = await fetch(apiUrl);
       const data = await res.json();
       const embed = new EmbedBuilder()
         .setImage(data.message)
         .setColor("Random");
-      await interaction.reply({ embeds: [embed] });
+      await interaction.reply({ embeds: [embed], components: [button] });
     } catch (error) {
       console.error("Error:", error);
       interaction.reply({ content: "An error occurred.", ephemeral: true });
