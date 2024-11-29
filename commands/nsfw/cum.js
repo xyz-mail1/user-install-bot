9d63ce455a1c3b3c2e40b2e79e60a211305e78b1
xyz-mail1 <sunday.september@protonmail.com>
2024-11-29 21:03:49 +0530

    cum

---
+++ commands/nsfw/cum.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("cum")
+    .setDescription("cum")
+    .addUserOption((option) =>
+      option.setName("person").setDescription(`The person to cum in~`)
+    )
+    .setIntegrationTypes([0, 1])
+    .setContexts([0, 1, 2]),
+  /**
+   * Executes the command when invoked by the user.
+   * @param {import('discord.js').Interaction} interaction - The interaction object provided by Discord when the command is triggered.
+   * @returns {Promise<void>} A promise that resolves when the interaction response is sent.
+   */
+  async execute(interaction) {
+    const response = await fetch("https://purrbot.site/api/img/nsfw/cum/gif");
+    if (!response.ok) {
+      return console.log("Network response was not ok");
+    }
+    const data = await response.json();
+
+    const target = interaction.options.getUser("person");
+    const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setDescription(`**${interaction.user.displayName}** is cumming ngh~`)
+      .setImage(data.link);
+    if (!target) {
+      await interaction.reply({ embeds: [embed] });
+    } else {
+      await interaction.reply({ content: `${target}`, embeds: [embed] });
+    }
+  },
+};
