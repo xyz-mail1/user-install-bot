f0e639079fffc71464b06e6efb9ebb7668ef1e09
xyz-mail1 <sunday.september@protonmail.com>
2024-12-03 18:36:11 +0530

    dont icrement db count on error

--- commands/nsfw/blowjob.js
+++ commands/nsfw/blowjob.js
@@ -18,27 +18,42 @@ module.exports = {
   async execute(interaction) {
     try {
       await interaction.deferReply();
+
       const target = interaction.options.getUser("person");
       const link = jsonArray[Math.floor(Math.random() * jsonArray.length)];
       const [senderID, receiverID] = [interaction.user.id, target.id].sort();
       const model = db("blowjob");
-      const result = await model.findOneAndUpdate(
+      const getModel = await model.findOne({
+        type: "blowjob",
+        senderID,
+        recieverID: receiverID,
+      });
+
+      // If no document exists, default to count 0
+      const localCount = (getModel?.count || 0) + 1;
+
+      const embed = new EmbedBuilder()
+        .setAuthor({
+          name: `${interaction.user.username} is sucking ${target.username} uwaa`,
+          iconURL: `${interaction.user.avatarURL()}`,
+        })
+        .setColor("Random")
+        .setImage(link)
+        .setDescription(`-# That's ${localCount} blowjobs now!`);
+      await interaction.editReply({
+        content: `${target}`,
+        embeds: [embed],
+      });
+      await model.findOneAndUpdate(
         { type: "blowjob", senderID, recieverID: receiverID },
         { $inc: { count: 1 } },
         { new: true, upsert: true }
       );
-      const embed = new EmbedBuilder()
-        .setColor("Random")
-        .setImage(link)
-        .setDescription(
-          `**${interaction.user.username}** is sucking **${target.username}** uwaa\n-# That's ${result.count} blowjobs now!`
-        );
-      await interaction.editReply({ content: `${target}`, embeds: [embed] });
     } catch (error) {
       await interaction.editReply({
         content: "there was an error while running the command",
-        ephemeral: true,
       });
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // wait 1 second before deleting the reply
       return console.log(error);
     }
   },
