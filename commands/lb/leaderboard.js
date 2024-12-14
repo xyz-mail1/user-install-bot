03634b8741673e19d9da34d342bd7bd2a388b82f
xyz-mail1 <sunday.september@protonmail.com>
2024-12-14 18:00:45 +0530

    finish the lb cmd

--- commands/lb/leaderboard.js
+++ commands/lb/leaderboard.js
@@ -4,12 +4,48 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("leaderboard")
-    .setDescription("fdshdfgh"),
+    .setDescription("fdshdfgh")
+    .addStringOption((option) =>
+      option
+        .setName("type")
+        .setDescription("type of command to search for")
+        .setRequired(true)
+        .setAutocomplete(true)
+    )
+    .setIntegrationTypes([0, 1])
+    .setContexts([0, 1, 2]),
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    const choices = [
+      "sixnine",
+      "anal",
+      "blowjob",
+      "fuck",
+      "pussylick",
+      "spank",
+      "cuddle",
+      "hug",
+      "kiss",
+      "lick",
+      "pat",
+      "poke",
+      "slap",
+    ];
+    const filtered = choices.filter((choice) =>
+      choice.startsWith(focusedValue)
+    );
+    await interaction.respond(
+      filtered.map((choice) => ({ name: choice, value: choice }))
+    );
+  },
   async execute(interaction) {
     try {
       await interaction.deferReply();
-      const model = db("hug");
-      const topHugs = await model.find().sort({ count: -1 }).limit(5);
+      const string = interaction.options.getString("type");
+      //console.log(string);
+
+      const model = db(string);
+      const topHugs = await model.find().sort({ count: -1 }).limit(10);
       const lb = await Promise.all(
         topHugs.map(async (entry, index) => {
           const sender = await interaction.client.users
@@ -20,14 +56,12 @@ module.exports = {
             .catch(() => null);
           return `${index + 1}. **${sender?.displayName || "unknown"}** ➜ **${
             receiver?.displayName || "unknown"
-          }**: ${entry.count} hugs`;
+          }**: ${entry.count} ${string}s`;
         })
       );
 
       const embed = new EmbedBuilder().setDescription(lb.join("\n"));
       await interaction.editReply({ embeds: [embed] });
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) {}
   },
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -36,6 +36,21 @@ module.exports = {
           console.log(error);
         }
       }
+    } else if (interaction.isAutocomplete()) {
+      const command = interaction.client.commands.get(interaction.commandName);
+
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+
+      try {
+        await command.autocomplete(interaction);
+      } catch (error) {
+        console.error(error);
+      }
     }
     const command = interaction.client.commands.get(interaction.commandName);
 
